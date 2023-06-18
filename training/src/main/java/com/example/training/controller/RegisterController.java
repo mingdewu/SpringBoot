9b66a5a21aad95e86a167bcac552cc6a8b8b0dfa
mingdebaba <51104897+mingdewu@users.noreply.github.com>
2023-06-18 20:10:43 +0800

    backend side update

--- training/src/main/java/com/example/training/controller/RegisterController.java
+++ training/src/main/java/com/example/training/controller/RegisterController.java
@@ -1,34 +1,32 @@
-package com.studytonight.controllers; 
-
-import javax.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import com.example.models.User;
-import com.example.service.UserService;
-import com.example.training.entity.UserInfo;
-
-@Controller
-public class RegisterController{
-
-	@Autowired 
-	private UserInfo userInfo;
-	
-	@GetMapping("/")
-	public String index() {
-		return "register";
-	}
-	
-	@PostMapping("save")
-	public String save(@Valid @ModelAttribute User user, BindingResult result) {
-		if(result.hasErrors()) {
-			return "register";
-		}else {
-			 userService.register(user);
-			 return "successful";
-		}	
-	}	
-}
+//package com.studytonight.controllers;
+//
+//import com.example.models.User;
+//import com.example.training.entity.UserInfo;
+//import jakarta.validation.Valid;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.validation.BindingResult;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.ModelAttribute;
+//import org.springframework.web.bind.annotation.PostMapping;
+//@Controller
+//public class RegisterController{
+//
+//	@Autowired
+//	private UserInfo userInfo;
+//
+//	@GetMapping("/")
+//	public String index() {
+//		return "register";
+//	}
+//
+//	@PostMapping("save")
+//	public String save(@Valid @ModelAttribute User user, BindingResult result) {
+//		if(result.hasErrors()) {
+//			return "register";
+//		}else {
+//			 userService.register(user);
+//			 return "successful";
+//		}
+//	}
+//}
